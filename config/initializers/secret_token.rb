25feafdea0ff7ae7a6a5aecc8dee669ca23e2ff6
Nikolaev Artem N <nuriko@inbox.ru>
2013-01-23 15:48:00 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewChebitRu::Application.config.secret_token = 'cf93da07ce2dda449bf34c97f2b9e416604ee5b189caa89a5ed624e03698fa6adc4168adfbb505f13262674ff13aeae7dc8a779592847f4baca6643e2112eab5'
