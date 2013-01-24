793b92ab5d5742c5fe5400c6cb58b71b6c1575a5
Nikolaev Artem N <nuriko@inbox.ru>
2013-01-24 10:35:37 +0400

    Удаляем лишние ридми

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module NewChebitRu
       g.helper_specs false
     end
 
+
+    config.i18n.default_locale = :ru
+    #config.i18n.available_locales = [:en, :ru]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
