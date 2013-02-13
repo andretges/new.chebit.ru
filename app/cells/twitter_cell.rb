81665907ad46a3d460f373d656c7c30b1b55cead
Nikolaev Artem N <nuriko@inbox.ru>
2013-02-13 13:53:21 +0400

    [#43826429] Забыл вернуть кеш на 10 минут

--- app/cells/twitter_cell.rb
+++ app/cells/twitter_cell.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class TwitterCell < Cell::Rails
 
-  #cache :show, :expires_in => 1.minutes
+  cache :show, :expires_in => 10.minutes
 
   def show
     render :locals => { :twitter => twitter = Twitter.search("#чебоксары -rt", :count => 10).results }
