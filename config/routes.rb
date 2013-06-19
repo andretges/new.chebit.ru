55bde3b3a30f41e383e7fe69cf141f65719acfb0
heretge <vozdelaluz@gmail.com>
2013-06-20 00:46:05 +0400

    new header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 NewChebitRu::Application.routes.draw do
-  ActiveAdmin.routes(self)
-
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :conferences, :only => [:index, :show]
   resources :presentations, :only => [:show, :index]
 
   root :to => "conferences#index"
+  ActiveAdmin.routes(self)
 end
